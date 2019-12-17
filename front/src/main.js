7c1451e2038b67a61ce1b70fc867f23bbe02763a
Martin Kagamino Lehoux <martin@lehoux.net>
2019-12-17 15:18:17 +0100

    feat - Chart for user records

--- front/src/main.js
+++ front/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import axios from 'axios'
 import SuiVue from 'semantic-ui-vue'
+import VueGoogleCharts from 'vue-google-charts'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
@@ -10,11 +11,12 @@ Vue.config.productionTip = false
 
 Vue.prototype.$http = axios.create({
   baseURL: `http://${window.location.hostname}:5000`,
-  timeout: 1000,
+  timeout: 5000,
   responseType: 'json'
 })
 
 Vue.use(SuiVue)
+Vue.use(VueGoogleCharts)
 
 new Vue({
   router,
--- front/src/views/UserDetail.vue
+++ front/src/views/UserDetail.vue
@@ -1,20 +1,26 @@
 <template lang="pug">
 div
   h1 {{user.username}}
-  table
-    thead
-      tr
-        th(@click="orderRecords('chrono')") Chrono
-        th(@click="orderRecords('timestamp')") Date
-    tbody
-      tr(v-for="record in records")
-        td {{(record.chrono / 1000).toFixed(3)}}
-        td {{record.timestamp.toLocaleString()}}
-  form(@submit.prevent="createRecord")
+  GChart(type="ScatterChart" :data="chartData" :options="chartOptions" v-if="records.length>0")
+  sui-table(single-line)
+    sui-table-header
+      sui-table-row
+        sui-table-header-cell(@click="orderRecords('chrono')") Chrono
+        sui-table-header-cell(@click="orderRecords('date')") Date
+    sui-table-body
+      sui-table-row(v-for="record in records" :key="record.id")
+        sui-table-cell {{(record.chrono / 1000).toFixed(2)}}
+        sui-table-cell {{record.date}}
+  form(@submit.prevent="createRecord(newRecord)")
     button(@click.prevent="startChrono()") Start
     button(@click.prevent="stopChrono()") Stop
-    p {{(newRecord.chrono / 1000).toFixed(3)}}
+    p {{(newRecord.chrono / 1000).toFixed(2)}}
     input(type="submit" value="Ajouter")
+  p OR
+  form(@submit.prevent="createRecord(newCustomRecord)")
+    input(type="number" step="0.001" v-model.number="newCustomRecord.chrono" placeholder="Chrono")
+    input(type="date" v-model="newCustomRecord.date")
+    input(type="submit" value="Create")
 </template>
 
 <script>
@@ -26,7 +32,19 @@ export default {
       user: new User({}),
       records: [],
       newRecord: new Record({}),
-      interval: null
+      newCustomRecord: new Record({}),
+      interval: null,
+      chartOptions: {
+        chart: {
+          title: 'Records',
+          legend: { position: 'bottom' }
+        }
+      }
+    }
+  },
+  computed: {
+    chartData () {
+      return [['Date', 'Chrono']].concat(this.records.map(record => [record.date, record.chrono]))
     }
   },
   created () {
@@ -38,22 +56,23 @@ export default {
       .catch(err => alert(err))
   },
   methods: {
-    createRecord () {
-      this.$http.post(`users/${this.$route.params.username}/records`, this.newRecord)
+    createRecord (record) {
+      this.$http.post(`users/${this.$route.params.username}/records`, record)
         .then(res => this.records.push(new Record(res.data)))
         .catch(err => alert(err))
       this.newRecord = new Record({})
+      this.newCustomRecord = new Record({})
     },
     orderRecords (order) {
       switch (order) {
         case 'chrono':
           this.records.sort((a, b) => a.chrono > b.chrono)
           break
-        case 'timestamp':
-          this.records.sort((a, b) => a.timestamp < b.timestamp)
+        case 'date':
+          this.records.sort((a, b) => a.date < b.date)
           break
         default:
-          this.records.sort((a, b) => a.timestamp < b.timestamp)
+          this.records.sort((a, b) => a.date < b.date)
           break
       }
     },

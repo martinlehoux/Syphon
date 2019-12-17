43925b919af07f3bad0663e28976610789396559
Martin Kagamino Lehoux <martin@lehoux.net>
2019-12-17 21:22:39 +0100

    test - Add test for models

--- back/test_models.py
+++ back/test_models.py
@@ -1,10 +1,14 @@
 from datetime import datetime, timedelta
-
+from peewee import SqliteDatabase
 from models import Record, User
+
+def teardown_function():
+    User.delete().execute() # pylint: disable=no-value-for-parameter
+    Record.delete().execute() # pylint: disable=no-value-for-parameter
 
 def test_user():
     User.create(username="Kagamino")
 
 def test_record():
-    user = User(id=0, name="Kagamino")
-    Record(id=0, date=datetime.now().date(), chrono=timedelta(seconds=6), user=user)
+    user = User(username="Kagamino")
+    Record(date=datetime.now(), chrono=6000, user=user)

8d7be862e9230950c0b1e374a652e1699f0bdbd1
Martin Kagamino Lehoux <martin@lehoux.net>
2019-12-17 21:19:16 +0100

    feat - Add validators for data creation

--- back/app.py
+++ back/app.py
@@ -2,7 +2,8 @@
 from flask import Flask, jsonify, request, abort
 from flask_cors import CORS, cross_origin
 from kaga_logger import INFO, Logger, DEBUG
-from models import User, Record, DoesNotExist
+from validators import User, Record
+from peewee import DoesNotExist
 from sys import argv
 
 app = Flask(__name__)
@@ -10,6 +11,10 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 logger = Logger(DEBUG)
+
+@app.errorhandler(400)
+def bad_request_handler(err):
+    return jsonify(error=str(err)), 400
 
 # @app.errorhandler(404)
 # def page_not_found(err):
@@ -22,8 +27,11 @@ def user_list():
         users = User.select()
         return jsonify([user.json() for user in users])
     elif request.method == "POST":
-        user = User.create(**request.json)
-        return jsonify(user.json()), 201
+        try:
+            user = User.create(**request.json)
+            return jsonify(user.json()), 201
+        except AssertionError as err:
+            abort(400, err)
 
 @app.route('/users/<string:username>', methods=['GET'])
 @cross_origin()
@@ -33,7 +41,7 @@ def user_detail(username: str):
             user = User.get(User.username == username)
             return jsonify(user.json())
         except DoesNotExist:
-            return jsonify(error=f"user '{username}' not found"), 404
+            abort(404, f"user '{username}' not found")
 
 @app.route('/users/<string:username>/records', methods=['GET', 'POST'])
 @cross_origin()
@@ -42,9 +50,12 @@ def user_record_list(username: str):
         records = Record.select().where(Record.user == username)
         return jsonify([record.json() for record in records])
     if request.method == 'POST':
-        record = Record.create(**request.json, user=username)
-        record = Record.get_by_id(record.id)
-        return jsonify(record.json()), 201
+        try:
+            record = Record.create(**request.json, user=username)
+            record = Record.get_by_id(record.id)
+            return jsonify(record.json()), 201
+        except AssertionError as err:
+            abort(400, err)
 
 @app.route('/records', methods=['GET'])
 def record_list():

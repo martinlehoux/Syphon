68df76d6ef7ab15b7f915982768f4717aa56bc4b
Martin Kagamino Lehoux <martin@lehoux.net>
2019-12-18 11:47:00 +0100

    feat - Improved UserList

--- front/src/views/UserList.vue
+++ front/src/views/UserList.vue
@@ -1,11 +1,23 @@
 <template lang="pug">
 div
-  ol
-    li(v-for="user in users" :key="user.username")
-      router-link(:to="`/users/${user.username}`") {{user.username}}
-  form(@submit.prevent="createUser")
-    input(type="text" v-model="newUser.username" id="")
-    input(type="submit" value="New user")
+  sui-table
+    sui-table-header
+      sui-table-row
+        sui-table-header-cell(@click="orderUsers('username')") Username
+        sui-table-header-cell(@click="orderUsers('chrono')") Best record
+        sui-table-header-cell Distinction
+    sui-table-body
+      sui-table-row(v-for="user in users" :key="user.username")
+        sui-table-cell
+          router-link(:to="`/users/${user.username}`") {{user.username}}
+        sui-table-cell {{(user.bestRecord.chrono / 1000).toFixed(2)}} sec
+        sui-table-cell {{user.distinction}}
+  sui-segment
+    sui-form(@submit.prevent="createUser")
+      sui-form-field
+        label Username
+        input(type="text" v-model="newUser.username" id="")
+      sui-button(type="submit" basic positive) Create user
 </template>
 
 <script>
@@ -21,14 +33,28 @@ export default {
   created () {
     this.$http.get('users')
       .then(res => (this.users = res.data.map(user => new User(user))))
-      .catch(err => this.$store.commit('addError', err.response.data.error))
+      .then(() => this.users.forEach(user => user.loadRecords()))
+      .catch(err => this.$error(err))
   },
   methods: {
     createUser () {
       this.$http.post('users', this.newUser)
         .then(res => this.users.push(new User(res.data)))
-        .catch(err => this.$store.commit('addError', err.response.data.error))
-      this.newUser = new User({})
+        .then(() => this.$router.push(`/users/${this.newUser.username}`))
+        .catch(err => this.$error(err))
+        .then(() => (this.newUser = new User({})))
+    },
+    orderUsers (order) {
+      switch (order) {
+        case 'chrono':
+          this.users.sort((a, b) => a.bestRecord.chrono > b.bestRecord.chrono)
+          break
+        case 'username':
+          this.users.sort((a, b) => a.username > b.username)
+          break
+        default:
+          break
+      }
     }
   }
 }

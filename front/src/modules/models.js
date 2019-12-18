41e7bed2fec965628084883bfa50e555fc546f5c
Martin Kagamino Lehoux <martin@lehoux.net>
2019-12-18 11:46:36 +0100

    feat - Move features ton User model

--- front/src/modules/models.js
+++ front/src/modules/models.js
@@ -1,13 +1,41 @@
+import http from '@/modules/http'
+
 export class User {
   username = ''
+  records = []
 
-  constructor ({ username }) {
+  constructor ({ username, records }) {
     this.username = username
+    if (records) this.records = records
+  }
+
+  loadRecords () {
+    http.get(`users/${this.username}/records`)
+      .then(res => (this.records = res.data.map(record => new Record(record))))
+  }
+
+  get bestRecord () {
+    return this.records.concat().sort((a, b) => a.chrono > b.chrono).shift() || {}
+  }
+
+  get distinction () {
+    const bestChrono = this.bestRecord.chrono
+    return [
+      'impossible',
+      'Unphon',
+      'Deuxphon',
+      'Triphon',
+      'Quadriphon',
+      'Cinqphon',
+      'Syphon',
+      'Septphon',
+      'Huitphon'
+    ][Math.ceil(bestChrono / 1000)] || 'No distinction'
   }
 }
 
 export class Record {
-  date = ''
+  date = new Date().toISOString().slice(0, 10)
   chrono = 0
 
   constructor ({ date, chrono, id, user }) {

7525b5d5a0dd150d84a602823a772deef9fd70bb
Martin Kagamino Lehoux <martin@lehoux.net>
2019-12-29 02:58:52 +0100

    feat - Better Axios error handling

--- front/src/modules/error.js
+++ front/src/modules/error.js
@@ -1,6 +1,11 @@
 import store from '@/store'
 
 export default function error (err) {
-  if (err.message) store.commit('addError', err.message)
+  // TODO: Handle 401 to fetch new token
+  if (err.response) {
+    store.commit('addError', err.response.data.error)
+  } else if (err.message) {
+    store.commit('addError', err.message)
+  }
   console.log(err)
 }

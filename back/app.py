75141cd4c801e88c0fdde38e219cc7f6945e6dce
Martin Kagamino Lehoux <martin@lehoux.net>
2019-12-29 02:58:35 +0100

    feat - Add JWT authorization method and protect routes

--- back/app.py
+++ back/app.py
@@ -1,11 +1,16 @@
 #! ./env/bin/python
 from sys import argv
+from time import time
 
+import jwt
 from flask import Flask, abort, jsonify, request
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
+from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
 from kaga_logger import DEBUG, Logger
 from peewee import DoesNotExist, IntegrityError
+from werkzeug.exceptions import HTTPException
 
+from conf import JWT_EXPIRES_IN, JWT_SECRET_KEY, PAGE_SIZE
 from validators import Record, User
 
 APP = Flask(__name__)
@@ -13,22 +18,60 @@ CORS = CORS(APP)
 APP.config['CORS_HEADERS'] = 'Content-Type'
 
 LOGGER = Logger(DEBUG)
-PAGE_SIZE = 10
 
+def token_protected(route):
+    def wrapper(*args, **kwargs):
+        authorization = request.headers.get('Authorization')
+        if not authorization:
+            abort(401, f"No Authentication method provided")
+        method = authorization.split()[0]
+        if method != "Bearer":
+            abort(400, f"Authorization method '{method}' is not supported, use 'Bearer'")
+        if len(authorization.split()) != 2:
+            abort(400, f"No token provided for 'Bearer' authorization method")
+        jwt_token = authorization.split()[1]
+        try:
+            request.data = jwt.decode(jwt_token, JWT_SECRET_KEY, algorithms='HS512')
+        except ExpiredSignatureError:
+            abort(401, "Token expired")
+        except InvalidTokenError:
+            abort(400, "Token is tampered")
+        return route(*args, **kwargs)
+    wrapper.__name__ = route.__name__
+    return wrapper
 
-@APP.errorhandler(400)
-def bad_request(err):
-    return jsonify(error=str(err)), 400
+#498 	Token expired/invalid
 
-@APP.errorhandler(404)
-def page_not_found(err):
-    return jsonify(error=str(err)), 404
+@APP.errorhandler(HTTPException)
+def http_exception(err):
+    return jsonify(error=str(err)), err.code
+
+@APP.route('/token', methods=['POST'])
+def token():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+    if not username:
+        abort(400, "username is required")
+    if not password:
+        abort(400, "password is required")
+    try:
+        user = User.get(User.username == username)
+        if user.password_hash is None:
+            abort(403, f"User '{username} doesn't have a password set and can't log in")
+        if not user.check_password(password):
+            abort(400, f"Password is wrong for User '{username}'")
+        jwt_token = jwt.encode(dict(exp=int(time()) + JWT_EXPIRES_IN, username=username), JWT_SECRET_KEY, algorithm='HS512')
+        return jsonify(token=jwt_token.decode('utf-8')), 201
+    except DoesNotExist:
+        abort(404, f"User '{username}' not found")
+
 
 @APP.route('/users', methods=['POST', 'GET'])
-@cross_origin()
+@token_protected
 def user_list():
     if request.method == "GET":
-        page = int(request.args.get('page')) or 0
+        page = int(request.args.get('page') or 0)
         users = User.select().order_by(User.username).offset(page * PAGE_SIZE).limit(PAGE_SIZE)
         return jsonify([user.json() for user in users])
     if request.method == "POST":
@@ -39,7 +82,7 @@ def user_list():
             abort(400, err)
 
 @APP.route('/users/<string:username>', methods=['GET'])
-@cross_origin()
+@token_protected
 def user_detail(username: str):
     if request.method == 'GET':
         try:
@@ -49,7 +92,7 @@ def user_detail(username: str):
             abort(404, f"User '{username}' not found")
 
 @APP.route('/users/<string:username>/records', methods=['GET', 'POST'])
-@cross_origin()
+@token_protected
 def user_record_list(username: str):
     if request.method == 'GET':
         records = Record.select().where(Record.user == username)
@@ -69,6 +112,7 @@ def record_list():
         return jsonify([record.json() for record in records])
 
 @APP.route('/records/<int:record_id>', methods=['DELETE'])
+@token_protected
 def record_detail(record_id: int):
     if request.method == 'DELETE':
         try:

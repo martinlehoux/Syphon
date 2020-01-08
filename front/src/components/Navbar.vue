4127d86426d7a7af467752c6ae40b7ec31bbd93a
Martin Kagamino Lehoux <martin@lehoux.net>
2020-01-08 01:31:41 +0100

    feat - Navbar adapts to loggin

--- front/src/components/Navbar.vue
+++ front/src/components/Navbar.vue
@@ -2,9 +2,10 @@
 sui-menu(id="nav" :widths="4")
   sui-menu-item(header v-if="loggedIn") Welcome {{username}}
   sui-menu-item(header v-else) Please log in
-  router-link(to="/admin" is="sui-menu-item" :active="$route.path == '/admin'") Admin &nbsp;
-  router-link(to="/records" is="sui-menu-item" :active="$route.path == '/records'") Records &nbsp;
-  router-link(to="/signup" is="sui-menu-item" :active="$route.path == '/signup'") Sign up &nbsp;
+  router-link(to="/" is="sui-menu-item" :active="$route.path == '/'") Home
+  router-link(to="/admin" is="sui-menu-item" :active="$route.path == '/admin'" :disabled="!isAdmin") Admin
+  router-link(v-if="!isAdmin" to="/signup" is="sui-menu-item" :active="$route.path == '/signup'") Sign up
+  sui-menu-item(v-else @click="$store.dispatch('logout')") Logout
 </template>
 
 <script>
@@ -12,7 +13,7 @@ import { mapState } from 'vuex'
 
 export default {
   name: 'Navbar',
-  computed: mapState(['loggedIn', 'username'])
+  computed: mapState(['loggedIn', 'username', 'isAdmin'])
 }
 </script>
 

445cd7e26fec1209807e89e48e88b06ac29d770e
Martin Kagamino Lehoux <martin@lehoux.net>
2020-01-08 02:07:37 +0100

    fix - Remove console.log for production

--- front/src/modules/error.js
+++ front/src/modules/error.js
@@ -1,7 +1,6 @@
 import store from '@/store'
 
 export default function error (err) {
-  console.log(err)
   if (err.response) {
     store.dispatch('addError', err.response.data.error)
   } else if (err.message) {
